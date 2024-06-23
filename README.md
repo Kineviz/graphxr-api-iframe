155cf9b030c19725e9dcf4c5642ad15a8efb2b14
beng <ben@kineviz.com>
2024-06-23 13:32:40 -0400

    v1.0.8

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ import gxr from '@kineviz/graphxr-api-iframe'
 // <iframe id="iframe" ...>
 gxr('iframe').graph().add('a', 'b');
 ```
+
+# Publishing
+
+```
+yarn login
+yarn publish
+```
--- src/index.js
+++ src/index.js
@@ -1,45 +1,40 @@
-export default function (iframeId) {
-  const task = {isRunning:false};
+export default function (selector = '#graphxr-iframe') {
+  const task = {};
   let queue = [];
 
-  const run = () => new Promise((resolve) => {
-    task.isRunning = true;
-    const iframe = document.getElementById(iframeId);
+  const run = (resolve) => {
+    const iframe = document.querySelector(selector);
     const iframeWindow = iframe.contentWindow;
     const origin = new URL(iframe.src).origin;
 
     // Wait for a response from GraphXR
     const listener = async (e) => {
-      if (e.data._type === 'gxr') {
-        window.removeEventListener('message', listener);
+      if (e.data._type === "gxr") {
+        window.removeEventListener("message", listener);
         resolve(e.data.result);
       }
-    }
-    window.addEventListener('message', listener);
+    };
+    window.addEventListener("message", listener);
 
-    // Send the queue of actions to GraphXR)
-    iframeWindow.postMessage({_type: "gxr", queue}, origin);
+    // Send the queue of actions to GraphXR
+    iframeWindow.postMessage({ _type: "gxr", queue }, origin);
     queue = [];
-  });
+  };
 
   const proxy = new Proxy(task, {
     get(target, action, receiver) {
-      if (action === 'isRunning') {
-        return task.isRunning;
-      }
-
-      if (action === "run") {
+      if (action === "then") {
         return run;
       }
 
       const wrapped = (...args) => {
-        args.forEach(arg => checkForFunctions(arg))
+        args.forEach((arg) => checkForFunctions(arg));
         queue.push({ action, args });
         return proxy;
       };
 
       return wrapped;
-    }
+    },
   });
 
   return proxy;
@@ -47,15 +42,12 @@ export default function (iframeId) {
 
 // Recursively walk an object and throw an error on any function
 const checkForFunctions = (obj) => {
-  if (!obj) return;
-
-  if (typeof obj === 'function') {
-    throw new Error('Cannot pass functions to iframe');
+  if (typeof obj === "function") {
+    throw new Error("Cannot pass functions to iframe");
   }
-
-  if (typeof obj === 'object') {
-    Object.keys(obj).forEach(key => {
+  if (typeof obj === "object") {
+    Object.keys(obj).forEach((key) => {
       checkForFunctions(obj[key]);
     });
   }
-}
+};
